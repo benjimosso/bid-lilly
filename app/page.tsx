7045115e2648559aec26e766483124dfe35f7932
benjimosso <benjimosso@gmail.com>
2024-07-03 19:28:32 -0700

    Changes to the Resend function and Winners logic

--- app/page.tsx
+++ app/page.tsx
@@ -38,52 +38,72 @@ export default async function HomePage() {
     const resend = new Resend(process.env.RESEND_API_KEY);
 
     const { data, error } = await resend.emails.send({
-      from: 'Bid For Lilly <team@bid-lilly.online>',
+      from: "Bid For Lilly <team@bid-lilly.online>",
       to: [email],
-      subject: 'You Won The Bid!',
-      react: EmailTemplate({ name, itemName, itemId, itemImage, amount})as React.ReactElement,
+      subject: "You Won The Bid!",
+      react: EmailTemplate({
+        name,
+        itemName,
+        itemId,
+        itemImage,
+        amount,
+      }) as React.ReactElement,
     });
     if (error) {
       return Response.json({ error }, { status: 500 });
     }
     console.log(data);
-    emailSent({ itemId: itemId}); 
+    emailSent({ itemId: itemId });
   }
 
-  let finalItems: {
+  let Winners: {
     name: string;
     amount: number;
-    item: itemInterface;
+    itemName: string;
     email: string;
+    itemId: number;
+    itemImage: string;
   }[] = [];
 
-  await Promise.all(
-    items.map(async (item: Item) => {
-      if (item.endDate < new Date().toISOString()) {
-        console.log(item.name, "Item is over");
-        await Promise.all(
-          bids?.map(async (bid: Bids) => {
-            if (
-              item.id === bid.item_id &&
-              item.currentBid === bid.amount &&
-              !item.emailSent
-            ) {
-              finalItems.push({
-                name: bid.full_name,
-                amount: bid.amount,
-                item: item,
-                email: bid.email,
-              });
-              await sendEmail(bid.full_name, item.name, bid.email, item.id, item.image, bid.amount);
-            } 
-          })
-        );
-      }
-    })
-  );
-  console.log(finalItems);
-  if (!user.user?.phone){
-    console.log("Please add your phone number to your account to receive notifications");
+  items.map(async (item: Item) => {
+    if (item.endDate < new Date().toISOString()) {
+      console.log(item.name, "Item is over");
+      bids?.map(async (bid: Bids) => {
+        if (
+          item.id === bid.item_id &&
+          item.currentBid === bid.amount &&
+          !item.emailSent
+        ) {
+          Winners.push({
+            name: bid.full_name,
+            amount: bid.amount,
+            itemId: item.id,
+            email: bid.email,
+            itemName: item.name,
+            itemImage: item.image,
+          });
+        }
+      });
+    }
+  });
+
+  if (Winners.length > 0) {
+    console.log("Winners", Winners);
+    Winners.map(async (winner) => {
+      sendEmail(
+        winner.name,
+        winner.itemName,
+        winner.email,
+        winner.itemId,
+        winner.itemImage,
+        winner.amount
+      );
+    });
+  }
+  if (!user.user?.phone) {
+    console.log(
+      "Please add your phone number to your account to receive notifications"
+    );
   }
   return (
     <div className="">
